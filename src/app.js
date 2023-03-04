0dedcf5bf880d9f8b9ff3b8544d521110a5da0e2
FasFoysal <shounfoysal@gmial.com>
2023-03-04 22:08:44 +0600

    cors add

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ var app = express();
 var router = require('./router/router')
 require('./../db/userData');
 const cookieParser = require("cookie-parser");
+var cors = require('cors');
 
 const PORT = process.env.PORT || 5050;
 
+app.use(cors({
+    origin:"https://splendorous-gumdrop-0c0736.netlify.app/"
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
